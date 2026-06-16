eda3671ab1a2b1fb33b78c3f07544b607420f497
1 <icy@1deMac-mini.local>
2026-06-16 11:14:32 +0800

    initial import from https://github.com/icymoon911/three-mesh-bvh-three-mesh-bvh

---
+++ eslint.config.js
@@ -0,0 +1,108 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import vitest from '@vitest/eslint-plugin';
+import globals from 'globals';
+import mdcs from 'eslint-config-mdcs';
+import jsdoc from 'eslint-plugin-jsdoc';
+
+export default [
+	// files to ignore
+	{
+		name: 'files to ignore',
+		ignores: [
+			'**/*.generated.js',
+			'**/node_modules/**',
+			'**/build/**',
+		],
+	},
+
+	// recommended
+	js.configs.recommended,
+
+	// base rules
+	{
+		name: 'base rules',
+		files: [ '**/*.js', '**/*.ts' ],
+		languageOptions: {
+			ecmaVersion: 2020,
+			sourceType: 'module',
+			globals: {
+				...globals.browser,
+				...globals.node,
+			},
+		},
+		rules: {
+			...mdcs.rules,
+			'no-mixed-spaces-and-tabs': 'error',
+		},
+	},
+
+	// ts recommended
+	...tseslint.configs.recommended.map( config => ( {
+		...config,
+		files: [ '**/*.ts' ],
+	} ) ),
+
+	// ts rule overrides
+	{
+		name: 'ts rule overrides',
+		files: [ '**/*.ts' ],
+		rules: {
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': 'off',
+		},
+	},
+
+	// jsdoc
+	{
+		name: 'jsdoc rules',
+		files: [ '**/*.js' ],
+		plugins: {
+			jsdoc,
+		},
+		settings: {
+			jsdoc: {
+				preferredTypes: {
+					Any: 'any',
+					Boolean: 'boolean',
+					Number: 'number',
+					object: 'Object',
+					String: 'string',
+				},
+				tagNamePreference: {
+					return: 'returns',
+					augments: 'extends',
+					classdesc: false,
+				},
+			},
+		},
+		rules: {
+			'jsdoc/check-tag-names': [ 'error', { definedTags: [ 'warn', 'note', 'section' ] } ],
+			'jsdoc/check-types': 'error',
+			'jsdoc/no-undefined-types': 'error',
+			'jsdoc/require-param-type': 'error',
+			'jsdoc/require-returns-type': 'error',
+			'jsdoc/require-returns': 'off',
+			'jsdoc/require-param-description': 'off',
+			'jsdoc/require-returns-description': 'off',
+		},
+	},
+
+	// vitest
+	{
+		name: 'vitest rules',
+		files: [ '**/*.test.js', '**/*.test.ts' ],
+		plugins: {
+			vitest,
+		},
+		languageOptions: {
+			globals: {
+				...vitest.environments.env.globals,
+			},
+		},
+		rules: {
+			...vitest.configs.recommended.rules,
+			'vitest/valid-describe-callback': 0,
+		},
+	},
+];
